2537f801d65568cf89cde5c7ead6505a9b138bf1
Hoang An <contact@unicode.vn>
2023-06-13 12:36:08 +0700

    Update code

--- hocjs/dom/example/carousel/js/script.js
+++ hocjs/dom/example/carousel/js/script.js
@@ -1,6 +1,11 @@
 var carousel = document.querySelector(".carousel");
 var carouselInner = document.querySelector(".carousel__inner");
 
+var carouselControls = carousel.querySelector(".carousel__controls");
+
+var carouselControlsPrev = carouselControls.children[0];
+var carouselControlsNext = carouselControls.children[1];
+
 var carouselInnerWidth = carouselInner.clientWidth;
 
 var carouselItems = carouselInner.children;
@@ -9,3 +14,28 @@ var totalWidth = carouselItems.length * carouselInnerWidth;
 
 carouselInner.style.width = `${totalWidth}px`;
 carouselInner.style.transition = `all 0.4s linear`;
+
+var translateCarousel = function (position) {
+  var positionWidth = 0 - carouselInnerWidth * position;
+  carouselInner.style.transform = `translateX(${positionWidth}px)`;
+};
+
+var position = 0;
+
+carouselControlsNext.addEventListener("click", function () {
+  if (position === carouselItems.length - 1) {
+    return;
+  }
+  position++;
+
+  translateCarousel(position);
+});
+
+carouselControlsPrev.addEventListener("click", function () {
+  if (position === 0) {
+    return;
+  }
+  position--;
+
+  translateCarousel(position);
+});
---
+++ hocjs/dom/example/player/js/script.js
@@ -0,0 +1,102 @@
+var audio = document.querySelector("audio");
+
+var player = document.querySelector(".player");
+
+var playerAction = player.querySelector(".player__action span");
+
+var durationEl = player.querySelector(".duration");
+
+var currentEl = player.querySelector(".current");
+
+var timer = player.querySelector(".timer input");
+
+var volume = player.querySelector(".volume");
+
+var playBtn = `<i class="fa-solid fa-play fa-2x"></i>`;
+
+var pauseBtn = `<i class="fa-solid fa-pause fa-2x"></i>`;
+
+var duration = 0;
+
+var isSeeking = false;
+
+var getTime = function (second) {
+  second = Math.floor(second);
+  var mins = Math.floor(second / 60);
+
+  second -= mins * 60;
+
+  return `${mins < 10 ? "0" + mins : mins}:${
+    second < 10 ? "0" + second : second
+  }`;
+};
+
+playerAction.addEventListener("click", function () {
+  if (audio.paused) {
+    audio.play();
+    this.innerHTML = pauseBtn;
+  } else {
+    audio.pause();
+    this.innerHTML = playBtn;
+  }
+});
+
+audio.addEventListener("loadeddata", function () {
+  duration = audio.duration;
+  durationEl.innerHTML = getTime(duration);
+});
+
+audio.addEventListener("timeupdate", function () {
+  var currentTime = audio.currentTime;
+
+  //Quy đổi currentTime => percent
+  var rate = (currentTime / duration) * 100;
+
+  if (!isSeeking) {
+    timer.value = rate;
+
+    currentEl.innerHTML = getTime(currentTime);
+  }
+});
+
+timer.addEventListener("input", function () {
+  isSeeking = true;
+
+  //Quy đổi percent thành currentTime
+  var currentTime = (this.value / 100) * duration;
+
+  currentEl.innerHTML = getTime(currentTime);
+});
+
+timer.addEventListener("change", function () {
+  var currentTime = (this.value / 100) * duration;
+  isSeeking = false;
+  audio.currentTime = currentTime;
+});
+
+volume.addEventListener("input", function () {
+  this.nextElementSibling.innerHTML = `${this.value}%`;
+  if (parseInt(this.value) === 0) {
+    this.previousElementSibling.innerHTML =
+      '<i class="fa-solid fa-volume-off"></i>';
+  } else {
+    this.previousElementSibling.innerHTML =
+      '<i class="fa-solid fa-volume-high"></i>';
+  }
+
+  audio.volume = this.value / 100;
+});
+
+volume.previousElementSibling.addEventListener("click", function () {
+  if (parseInt(audio.volume) > 0) {
+    this.innerHTML = '<i class="fa-solid fa-volume-off"></i>';
+    audio.volume = 0;
+    volume.value = 0;
+    volume.nextElementSibling.innerHTML = `0%`;
+  } else {
+    this.innerHTML = ' <i class="fa-solid fa-volume-high"></i>';
+    audio.volume = 1;
+    volume.value = 100;
+    volume.nextElementSibling.innerHTML = `100%`;
+  }
+});
